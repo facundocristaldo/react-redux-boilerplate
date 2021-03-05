f5637b7f8bb98ce70563f237fe8100cd957a2223
Facundo <facucristaldorodriguez@gmail.com>
2021-03-05 15:07:45 -0300

    comments at store file

--- src/state/store/store.js
+++ src/state/store/store.js
@@ -4,10 +4,14 @@ import thunk from "redux-thunk";
 
 import allReducers from '../reducers'
 
+//Initial State
 const initialState = {count:0, lastAction:undefined}
 
+//middleWare with thunk
 const middleWare = [thunk];
 
+//create store
 const store = createStore(allReducers,initialState,composeWithDevTools(applyMiddleware(...middleWare)))
 
+//export the store
 export default store;
